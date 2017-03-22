8f3086eadc3583a6640f9e2fedcbc667f60bc551
ibanez195 <ibanez195@gmail.com>
2017-03-22 09:43:43 -0400

    Fix quotation marks from gooogle drive

--- CreateTables.sql
+++ CreateTables.sql
@@ -190,18 +190,18 @@ ALTER TABLE WEB_ORDER   ADD CONSTRAINT fk_dis_ord FOREIGN KEY(Disp_ID)          
 
 INSERT INTO EMPLOYEE values
 (987, "Crain", "Chris", 123, 5, 1),
-(876, ‘Nelson’, ‘Sean’, 123, 5, 2),
-(123, ‘Niu’, ‘Charles’, 234, 5, 2),
-(234, ‘Krishnasamy’, ‘Perumal’, null, 6, 3);
+(876, "Nelson", "Sean", 123, 5, 2),
+(123, "Niu", "Charles", 234, 5, 2),
+(234, "Krishnasamy", "Perumal", null, 6, 3);
 
 INSERT INTO DEPARTMENT values
-(5, ‘Technology’,  123),
-(6, ‘Business’, 234);
+(5, "Technology",  123),
+(6, "Business", 234);
 
 INSERT INTO FACILITY values
-(1, 987, ‘3103 Columbus Drive, Columbus, OH’),
-(2, 876, ‘1926 Woodruff Ave, Cleveland, OH’),
-(3, 234, ‘222 Lane Ave, Cincinnati, OH’);
+(1, 987, "3103 Columbus Drive, Columbus, OH"),
+(2, 876, "1926 Woodruff Ave, Cleveland, OH"),
+(3, 234, "222 Lane Ave, Cincinnati, OH");
 
 INSERT INTO  STORES values
 (1, 2221, 512),
@@ -215,12 +215,12 @@ INSERT INTO WHOLESALE_ORDER values
 (5555, 3, 200);
 
 INSERT INTO PRODUCT values
-(32123437, ‘Dell’, 200, 97),
-(55445676, ‘Intel’, 225, 96),
-(55599778, ‘Corsair’, 70, 95),
-(66758678, ‘Razer’, 120, 94),
-(96707578, ‘Sony’, 540, 93),
-(85768374, ‘Toshiba’, 770, 92);
+(32123437, "Dell", 200, 97),
+(55445676, "Intel", 225, 96),
+(55599778, "Corsair", 70, 95),
+(66758678, "Razer", 120, 94),
+(96707578, "Sony", 540, 93),
+(85768374, "Toshiba", 770, 92);
 
 INSERT INTO W_CONTAINS values
 (3333, 32123437, 100),
@@ -229,66 +229,66 @@ INSERT INTO W_CONTAINS values
 (5555, 55599778, 300);
 
 INSERT INTO COMPUTER values
-(32123437, 12131415, ‘AMD’, 8, 1000),
-(55445676, 23242526, ‘INTEL’, 16, 512),
+(32123437, 12131415, "AMD", 8, 1000),
+(55445676, 23242526, "INTEL", 16, 512),
 
 INSERT INTO COMPUTER_MOUSE values
-(55599778, 34353637, 1200, ‘Braided’, ‘Optical’),
-(66758678, 45464748, 1800, ‘Wireless’, ‘Laser’);
+(55599778, 34353637, 1200, "Braided", "Optical"),
+(66758678, 45464748, 1800, "Wireless", "Laser");
 
 INSERT INTO TELEVISION values
-(96707578, 56575859, ‘Sony’, ‘LCD’, 46),
-(85768374, 67686960, ‘Toshiba’, ‘OLED’, 52);
+(96707578, 56575859, "Sony", "LCD", 46),
+(85768374, 67686960, "Toshiba", "OLED", 52);
 
 INSERT INTO AD values 
-(234, ‘www.budgetgadgets.com’, ‘Beats’, 2717, 112, 55599778),
-(345, ‘www.sony.com’, ‘Sony’, 3646, 651, 96707578),
-(456, ‘www.razer.com’, ‘Razer, 1122, 181, 66758678);
+(234, "www.budgetgadgets.com", "Beats", 2717, 112, 55599778),
+(345, "www.sony.com", "Sony", 3646, 651, 96707578),
+(456, "www.razer.com", "Razer", 1122, 181, 66758678);
 
 INSERT INTO WEBSITE values
-(‘www.budgetgadgets.com’, ‘Finland’, 3111),
-(‘www.sony.com’, ‘Japan’, 688868),
-(‘www.razer.com’, ‘US’, 21022);
+("www.budgetgadgets.com", "Finland", 3111),
+("www.sony.com", "Japan", 688868),
+("www.razer.com", "US", 21022);
 
 INSERT INTO MANUFACTURER values
-(97, ‘Dell’),
-(96, ‘Intel’),
-(95, ‘Corsair’),
-(94, ‘Razer’),
-(93, ‘Sony’),
-(92, ‘Toshiba’);
+(97, "Dell"),
+(96, "Intel"),
+(95, "Corsair"),
+(94, "Razer"),
+(93, "Sony"),
+(92, "Toshiba");
 
 INSERT INTO CUSTOMER values
-(‘bobgribben@gmail.com’, ‘Gribben’, ‘Bob’, ‘resetMypassw0rd’, 6145239272),
-(‘bobross@aim.com’, ‘Ross’, ‘Bob’, ‘alittl3mor3blu3’, 3392287409),
-(‘presidentdrake@osu.edu’, ‘Drake’, ‘President’, ‘123buckeyes’, 6145259384);
+("bobgribben@gmail.com", "Gribben", "Bob", "resetMypassw0rd", 6145239272),
+("bobross@aim.com", "Ross", "Bob", "alittl3mor3blu3", 3392287409),
+("presidentdrake@osu.edu", "Drake", "President", "123buckeyes", 6145259384);
 
 INSERT INTO CREDIT_CARD values
-(8112671562529948, 2017-11-11, ‘bobgribben@gmail.com’),
-(3883777738387070, 2021-06-06, ‘bobross@aim.com’),
-(1111222233334444, 2019-01-01, ‘presidentdrake@osu.edu’);
+(8112671562529948, 2017-11-11, "bobgribben@gmail.com"),
+(3883777738387070, 2021-06-06, "bobross@aim.com"),
+(1111222233334444, 2019-01-01, "presidentdrake@osu.edu");
 
 INSERT INTO WEB_ORDER values
-(234234, 52, ‘bobgribben@gmail.com’, 614614),
-(345345, 15, ‘bobross@aim.com’, 416553);
+(234234, 52, "bobgribben@gmail.com", 614614),
+(345345, 15, "bobross@aim.com", 416553);
 
 INSERT INTO U_CONTAINS values
 (234234, 32123437, 1),
 (345345, 96707578, 2);
 
 INSERT INTO U_PLACES values
-(‘bobgribben@gmail.com’, 234234),
-(‘bobross@aim.com’, 345345);
+("bobgribben@gmail.com", 234234),
+("bobross@aim.com", 345345);
 
 INSERT INTO DISPATCHER values
-(416553, ‘UPS’),
-(614614, ‘FedEx’);
+(416553, "UPS"),
+(614614, "FedEx");
 
 INSERT INTO REVIEWS values
-(‘bobgribben@gmail.com’, 96707578, ‘It asked me to create a new password. Would recommend.’, 5),
-(‘bobross@aim.com’, 32123437, ‘Not impressed. I thought a Dell would sing.’, 2);
+("bobgribben@gmail.com", 96707578, "It asked me to create a new password. Would recommend.", 5),
+("bobross@aim.com", 32123437, "Not impressed. I thought a Dell would sing.", 2);
 
 INSERT INTO USER_ADDRESS values
-(‘bobgribben@gmail.com’, ‘1337 OSU Lane’),
-(‘bobross@aim.com’, ‘411 Painters Way’),
-(‘presidentdrake@osu.edu’, ‘614 Stadium Ave’);
+("bobgribben@gmail.com", "1337 OSU Lane"),
+("bobross@aim.com", "411 Painters Way"),
+("presidentdrake@osu.edu", "614 Stadium Ave");
