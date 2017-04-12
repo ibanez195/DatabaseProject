2444932317703777b8a004b4ff8a847e8bc09bd1
Sean Nelson <ibanez195@gmail.com>
2017-04-12 15:30:08 -0400

    delete false Queries

--- Queries.sql
+++
@@ -1,7 +0,0 @@
-SELECT LastName, FirstName, Email, total_spent
-FROM CUSTOMER
-INNER JOIN
-(SELECT User_email,SUM(OCost) as total_spent
- FROM WEB_ORDER 
- GROUP BY User_email) user_spent 
- ON CUSTOMER.Email = User_email;
